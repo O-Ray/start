554e66a57b8d0b39c9438644bb7b17c82347cae1
RayOtsuka <rayrayo8855@gmail.com>
2019-09-03 18:30:20 +0900

    Initial Commit

---
+++ start/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  start
+//
+//  Created by 大塚嶺 on 2019/09/03.
+//  Copyright © 2019 大塚嶺. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
